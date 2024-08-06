e2042c6704215d587249eeb6b307e4092ba7d7fb
hugopoggetti <hugo.poggetti@epitech.eu>
2024-08-06 17:08:07 +0200

    more exercise

--- src/threeSum.cpp
+++ src/threeSum.cpp
@@ -9,10 +9,33 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums)
     {
-        vector<vector<int>> res;
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        res.push_back({nums[0]});
-        return res;
+        vector<vector<int>> result;
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int l = i + 1;
+            int r = n - 1;
+
+            while (l < r) {
+                int sum = nums[l] + nums[r] + nums[i];
+
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[l], nums[r]});
+                    l++, r--;
+                    while (l < r && nums[l] == nums[l - 1])
+                        l++;
+                    while (l < r && nums[r] == nums[r + 1])
+                        r--;
+                } else if (sum < 0)
+                    l += 1;
+                else
+                    r -= 1;
+            }
+        }
+        return result;
     }
 };
 
